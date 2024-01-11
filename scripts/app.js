73d029e54705af878b5a7cc09308757b226cfc52
Nathan <antnefedov33@gmail.com>
2024-01-11 16:16:34 +0300

    filled the window with hexagons by java script

--- scripts/app.js
+++ scripts/app.js
@@ -1,1 +1,36 @@
 "use strict"
+
+let container = document.querySelector('.container');
+let AmountOfHexagonsByWidth, AmountOfHexagonsByHeight;
+let amountOfHexagons;
+
+function start() {
+    resizeReset();
+    createHexagons(AmountOfHexagonsByHeight, AmountOfHexagonsByWidth);
+}
+
+function resizeReset() {
+    let width = window.innerWidth;
+    let height = window.innerHeight;
+    AmountOfHexagonsByWidth = Math.floor(width / 100 + 1);
+    AmountOfHexagonsByHeight = Math.floor(height / 76 + 1);
+    amountOfHexagons = AmountOfHexagonsByWidth * AmountOfHexagonsByHeight;
+}
+
+function createHexagons(heightCount, widthCount) {
+
+    for (let i = 0; i < heightCount; i++) {
+        let row = document.createElement("div");
+        row.className = "row";
+
+        for (let j = 0; j < widthCount; j++) {
+            let hexagon = document.createElement("div");
+            hexagon.className = "hexagon";
+            row.appendChild(hexagon);
+        };
+
+        container.appendChild(row);
+    }
+}
+
+window.addEventListener('DOMContentLoaded', start);
